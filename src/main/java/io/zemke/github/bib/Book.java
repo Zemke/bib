d2689aafe3e17d39b56120d5c9b606c0f08a3155
Zemke <flzemke@gmail.com>
2024-08-21 13:05:02 +0200

    init

---
+++ src/main/java/io/zemke/github/bib/Book.java
@@ -0,0 +1,69 @@
+package io.zemke.github.bib;
+
+public class Book {
+    private String id;
+    private String name;
+    private String html;
+    private Boolean avail;
+
+    public Book(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Book setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getHtml() {
+        return html;
+    }
+
+    public Book setHtml(String html) {
+        this.html = html;
+        return this;
+    }
+
+    public Boolean getAvail() {
+        return avail;
+    }
+
+    public Book setAvail(Boolean avail) {
+        this.avail = avail;
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Book book = (Book) o;
+
+        return id.equals(book.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", html='" + html + '\'' +
+                ", avail='" + avail + '\'' +
+                '}';
+    }
+
+}
